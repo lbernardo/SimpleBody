b8ca9ebdd0163a79fd0ca5d4de948b7c62a34732
lbernardo <lucas080795@hotmail.com>
2014-11-28 14:35:04 -0200

    Adicionado Bootstrap

--- class/init.php
+++ class/init.php
@@ -6,6 +6,9 @@
          // Verifica 
          if(dirname(__FILE__)."/lib/".$class_name.".php"){
              include_once dirname(__FILE__)."/lib/".$class_name.".php";
+         }elseif(dirname(__FILE__)."/app/".$class_name.".php"){
+             include_once dirname(__FILE__)."/app/".$class_name.".php";
          }
+         
      }
      include 'SimpleBody.php';
